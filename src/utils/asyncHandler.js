46230d9585a2032c4f2b542b15ba8ef932e58b48
Ayush Saraswat <ayushsaraswat27@gmail.com>
2025-03-16 09:40:14 +0530

    Uploaded Files on Cloudinary

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,15 +5,7 @@ const asyncHandler = (requestHandler) => {
     }
 }
 
-
 export { asyncHandler }
-
-
-
-
-// const asyncHandler = () => {}
-// const asyncHandler = (func) => () => {}
-// const asyncHandler = (func) => async () => {}
 
 
 // const asyncHandler = (fn) => async (req, res, next) => {
